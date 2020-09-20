c99d6dd254f7725a74fbb486d34df851ab91a812
Christina Poole <codenamedchris@gmail.com>
2020-09-20 04:12:41 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -9,11 +9,9 @@ def nyc_pigeon_organizer(data)
   data.each do |key, value|
     value.each do |key2, value2|
       name_array.each do |name|
-        if key2.has_value?(name) do
-          data.replace({ name => { key => key2 }})
-        end
+        key2.has_value?(name)
+        data.replace({ name => { key => key2 }})
       end
     end
   end
-end
 end
